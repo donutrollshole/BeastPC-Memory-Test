1a12641c9c1b3381fb47fb8a7be009e4986aa8e0
donutrollshole <65697836+donutrollshole@users.noreply.github.com>
2021-06-16 23:10:03 -0400

    created GUI

--- latency_test_gui.py
+++ latency_test_gui.py
@@ -1,6 +1,8 @@
-def latency_test():
-    import time
+import tkinter as tk
+import time
 
+# Backbone algorithms for the latency test
+def latency_test():
     cycles = 2650  # This many cycles on M1 Macbook Air returns latency similar to AIDA
 
     # creating test list of length cycles
@@ -8,6 +10,7 @@ def latency_test():
     for i in range(cycles):
         test_list.append(str(i)) #random string
     
+    # cycles1 and cycles2 are chosen for a balance of runtime and precision
     # testing total latency time by finding average over cycles1 tries
     totallatencytime = 0
     cycles1 = 3000
@@ -45,4 +48,8 @@ def run_latency_test(iterations):
     avg_time = str(total_time/iterations)
     return avg_time
 
-print(run_latency_test(10) + 'ns')
+# GUI for the latency test
+
+root = tk.Tk
+
+root.mainloop
